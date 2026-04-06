c37464f57a7c5cda17fa5a1b958d45bb79c6e018
rohanvjoshi17-cyber <rohanvjoshi17@gmail.com>
2026-04-06 16:29:29 +0530

    activity1

--- activity1.py
+++ activity1.py
@@ -1,1 +1,18 @@
-print ("hello")
+name = "Rohan"
+age = 14
+weight = 60.3
+is_student = True
+
+print("Name is :", name)
+print("Data type of Name is", type(name))
+print("Age :", age)
+print("Data type of Age is", type(age))
+print("is_student :", is_student)
+print("Data type of is_student is", type(is_student))
+print("weight :", weight)
+print("Data type of weight is", type(weight))
+
+age = str(age)
+print("Data type of Age is", type(age))
+weight = int(weight)
+print("Data type of Weight is", type(weight))
