cc4e204d7c8390ea298f2e6474a44b44da1d939e
rohanvjoshi17-cyber <rohanvjoshi17@gmail.com>
2026-04-06 16:45:27 +0530

    activity2

---
+++ activity2.py
@@ -0,0 +1,11 @@
+num = 45
+num2 = 3
+
+print("Addition :", num+num2)
+print("Subtraction :", num-num2)
+print("Multiplication :", num*num2)
+print("Division :", num/num2)
+print("Floor Division :", num//num2)
+print("Modulas Operation :", num%num2)
+print("Square :", num**2)
+print("Sqaure Root :", num**0.5)
