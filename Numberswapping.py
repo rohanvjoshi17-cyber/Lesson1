c286bb5952612ddab19ba3803eef74f9fe2dde7a
rohanvjoshi17-cyber <rohanvjoshi17@gmail.com>
2026-04-07 18:29:19 +0530

    swapping numbers

---
+++ Numberswapping.py
@@ -0,0 +1,13 @@
+a=int(input("input number A :"))
+print("A is", a)
+b=int(input("input number B :"))
+print("B is", b)
+c=int(input("input number C :"))
+print("C is", c)
+swap=a
+a=b
+print("A has swapped with B and is now", a)
+b=c
+print("B has swapped with C and is now", b)
+c=swap
+print("C has swapped with A and is now", c)
