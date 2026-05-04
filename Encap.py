f277aba1a91eacfb1a927add4dba28f75f011abd
rohanvjoshi17-cyber <rohanvjoshi17@gmail.com>
2026-05-04 16:45:35 +0530

    file.py

--- Encap.py
+++ Encap.py
@@ -1,17 +1,17 @@
 class Computer:
     def __init__(self):
-        self.__maxprice = 900
+        self.maxprice = 900
     
     def sell(self):
-        print("Selling Price: {}".format(self.__maxprice))
+        print("Selling Price: {}".format(self.maxprice))
     
     def setMaxprice(self, price):
-        self.__maxprice = price
+        self.maxprice = price
 
 c = Computer()
 c.sell()
 
-c._maxprice = 1000
+c.maxprice = 1000
 c.sell()
 
 c.setMaxprice(1000)
---
+++ flie.py
@@ -0,0 +1,3 @@
+file = open('Codingle.txt')
+print(file.read())
+file.close()
