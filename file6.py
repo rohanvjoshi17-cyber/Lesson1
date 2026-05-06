6581fe73e831a585a01fff779c465a64cb859c8e
rohanvjoshi17-cyber <rohanvjoshi17@gmail.com>
2026-05-06 16:26:49 +0530

    file6.py

---
+++ file6.py
@@ -0,0 +1,18 @@
+file = open('Codingal.txt', 'r')
+print("Reading first line...")
+print(file.readline())
+file.close()
+
+file = open('Codingal.txt', 'r')
+print("Reading multiple lines...")
+print(file.readline())
+print(file.readline())
+print(file.readline())
+file.close()
+
+file = open('Codingal.txt', 'r')
+print("Reading multiple lines with loop...")
+for line in file:
+    print(line)
+file.close()
+
