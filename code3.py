d7299b1b5c9bd1bec3a328b1ebc7a011a764dfbe
rohanvjoshi17-cyber <rohanvjoshi17@gmail.com>
2026-05-22 20:34:01 +0530

    code3.py

---
+++ code3.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from PIL import Image, ImageTk
+
+root = Tk()
+root.title('image')
+root.geometry('400x400')
+
+upload = Image.open("img.jpg")
+image = ImageTk.PhotoImage(upload)
+
+label = Label(root, image=image, height=350, width=300)
+label.place(x=50, y=0)
+label12 = Label(root, text="This is how you add images in Tkinter Window")
+label12.place(x=40, y=360)
+
+root.mainloop()
